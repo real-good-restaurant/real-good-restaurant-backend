91a3d6affebf6cc0dcbc06ba64d622b4affc9441
이준석 <junzzang97@naver.com>
2022-05-24 20:33:18 +0900

    first commit

---
+++ src/main/java/com/mju/reviewclassifierjmj/ReviewClassifierJmjApplication.java
@@ -0,0 +1,13 @@
+package com.mju.reviewclassifierjmj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewClassifierJmjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewClassifierJmjApplication.class, args);
+    }
+
+}
